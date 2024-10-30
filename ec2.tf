bcf178d3503b617bd0c244465e15d2934934d5b4
uk <unnibb208@gmail.com>
2024-10-30 22:51:48 +1300

    added challenge.tf

--- ec2.tf
+++
@@ -1,5 +0,0 @@
-# Resource Block
-resource "aws_instance" "ec2demo-2" {
-  ami           = "ami-086918d8178bfe266" # RedHat Linux in Asia Pacific (Sydney) ap-southeast-2
-  instance_type = "t2.micro"
-}
--- timesleep.tf
+++
@@ -1,3 +0,0 @@
-resource "time_sleep" "wait_10_seconds" {
-   create_duration = "10s"
-}
