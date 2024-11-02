3793fdb5a1e6f3b8c280e008fde3a99ead440fee
Unnikrishnan K M <80303619+Unnikrishnan-K-M@users.noreply.github.com>
2024-11-02 18:32:12 +1300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,24 @@
 # Resource Block
-resource "aws_instance" "ec2demo-2" {
+resource "aws_instance" "Apache-WebServer-1" {
   ami           = "ami-086918d8178bfe266" # RedHat Linux in Asia Pacific (Sydney) ap-southeast-2
   instance_type = "t2.micro"
+  subnet_id = aws_subnet.dev-vpc-public-subnet-1.id
+  key_name = "terraform-key" #created manually before building EC2
+	user_data = file("apache-install.sh")
+  /*
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+    EOF  
+   */
+
+  vpc_security_group_ids = [ aws_security_group.dev-vpc-sg-1.id ]
+
+ tags = {
+    Name = "Apache-WebServer"
+  }
 }
