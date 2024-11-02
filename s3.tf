f123dc13fb4ddc20b10fe69a8477506ba91208db
uk <unnibb208@gmail.com>
2024-11-02 21:33:08 +1300

    updated s3 bucket name and subnet az

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-20"
+resource "aws_s3_bucket" "myexample-151974" {
+  bucket = "my-tf-test-bucket-151974"
 
   tags = {
     Name        = "My bucket"
@@ -7,15 +7,15 @@ resource "aws_s3_bucket" "example" {
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_ownership_controls" "myexample-151974" {
+  bucket = aws_s3_bucket.myexample-151974.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_public_access_block" "myexample-151974" {
+  bucket = aws_s3_bucket.myexample-151974.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -23,12 +23,12 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "mymyexample-151974" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
+    aws_s3_bucket_ownership_controls.myexample-151974,
+    aws_s3_bucket_public_access_block.myexample-151974,
   ]
 
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.myexample-151974.id
   acl    = "public-read"
 }
--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "dev-vpc-1" {
 resource "aws_subnet" "dev-vpc-public-subnet-1" {
   vpc_id                  = aws_vpc.dev-vpc-1.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-southeast-2-1a"
+  availability_zone       = "ap-southeast-2b"
   map_public_ip_on_launch = true
 
   tags = {
