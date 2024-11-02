fd327ee2652b1ba22c59baf1659b57ca1a23bf0d
Unnikrishnan K M <80303619+Unnikrishnan-K-M@users.noreply.github.com>
2024-11-02 18:18:30 +1300

    Create dev-vpc-sg.tf

---
+++ dev-vpc-sg.tf
@@ -0,0 +1,35 @@
+# Resource-7: Create Security Group
+resource "aws_security_group" "dev-vpc-sg-1" {
+  name        = "dev-vpc-sg"
+  vpc_id      = aws_vpc.dev-vpc-1.id
+  description = "Dev VPC Default Security Group"
+  
+ingress {
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow port 22"
+}
+
+ingress {
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow port 80"
+}
+
+egress {
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1"
+  cidr_blocks      = ["0.0.0.0/0"]
+  description       = "Allow all ip's & ports outbound"
+}
+
+tags = {
+    Name = "allow_ssh_http"
+  }
+
+}
